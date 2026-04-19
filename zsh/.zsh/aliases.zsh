3b42e31e3b73df1ec94aa244f70c4e2862eb3b35
Thijs Alberts <thijs@tsjeng.nl>
2026-04-19 13:48:57 +0200

    fix: shellcheck warnings in aliases.zsh

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -4,17 +4,15 @@
 alias week='date +%V'
 
 # Reload the shell (i.e. invoke as a login shell)
-alias reload="exec $SHELL -l"
-
-# Public IP address
-alias ip="curl -s ipinfo.io/ip && echo"
+alias reload='exec $SHELL -l'
 
 # Ring the terminal bell, and put a badge on Terminal.app's Dock icon (useful when executing time-consuming commands)
-alias badge="tput bel" 
+alias badge='tput bel'
 
 alias ff='find . -type f -name'
 alias h='history | grep' # Easy history grep
 
 if [[ "$(uname)" == "Darwin" ]]; then
+# shellcheck source=.zsh/aliases-osx.zsh
   source ~/.zsh/aliases-osx.zsh
 fi
